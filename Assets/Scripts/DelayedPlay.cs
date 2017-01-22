442d8d0d91c3c3cdcaaf668625964b2a486243f1
Shan Naziripour <shan@naziripour.com>
2017-01-22 14:02:32 -0800

    updates

---
+++ Assets/Scripts/DelayedPlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedPlay : MonoBehaviour {
+    public ulong delay = 44100;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().Play(delay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
