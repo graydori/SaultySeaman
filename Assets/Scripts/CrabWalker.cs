61318fccb8c2364aa4e6f091ba870caf25279b5b
David Gelbart <david.gelbart@gmail.com>
2017-02-07 22:41:22 -0800

    hoo ah

---
+++ Assets/Scripts/CrabWalker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CrabWalker : MonoBehaviour {
+
+    public float _UpDown;
+    public float _Angle;
+    public float _Period;
+
+    private float _Time;
+
+    // Update is called once per frame
+    void Update () {
+        _Time = _Time + Time.deltaTime;
+        float phase = Mathf.Sin(_Time / _Period);
+        transform.position += new Vector3(Math.Sign(phase) * _UpDown, 0, 0);
+    }
+}
