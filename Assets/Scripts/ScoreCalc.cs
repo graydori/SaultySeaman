0b6e42758b0beb9aaece6f1faf2b97756144a26a
David Gelbart <david.gelbart@gmail.com>
2017-02-07 22:26:41 -0800

    a sexy merman sternly judges your seductiveness

--- Assets/Scripts/ScoreCalc.cs
+++ Assets/Scripts/ScoreCalc.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class ScoreCalc : MonoBehaviour {
 
     public string continueText;
+
+    public AudioClip[] poorScore;
+    public AudioClip[] okScore;
+    public AudioClip[] goodScore;
     
 	// Use this for initialization
 	void Start () {
@@ -14,6 +18,17 @@ public class ScoreCalc : MonoBehaviour {
         if (total > 0) score = (float)UserState.success / total * 100;
 
         GetComponent<Text>().text = "seduction score: " + Mathf.Round(score) + "% \n\n " + continueText;
+
+        AudioClip[] clipArray;
+        if (score <= 33)
+            clipArray = poorScore;
+        else if (score <= 75)
+            clipArray = okScore;
+        else
+            clipArray = goodScore;
+        AudioClip clip = clipArray[Random.Range(0, clipArray.Length)];
+        GetComponent<AudioSource>().clip = clip;
+        GetComponent<AudioSource>().Play();
 
         // Clear for replay
         UserState.success = 0;
