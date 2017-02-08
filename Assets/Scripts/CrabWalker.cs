ee9a6106d3b2393bb4ef8122722c5079ad8dd27f
David Gelbart <david.gelbart@gmail.com>
2017-02-07 22:44:56 -0800

    dddd

--- Assets/Scripts/CrabWalker.cs
+++ Assets/Scripts/CrabWalker.cs
@@ -5,6 +5,8 @@ using System;
 
 public class CrabWalker : MonoBehaviour {
 
+    public Pointer pointer;
+
     public float _UpDown;
     public float _Angle;
     public float _Period;
@@ -13,8 +15,12 @@ public class CrabWalker : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        _Time = _Time + Time.deltaTime;
-        float phase = Mathf.Sin(_Time / _Period);
-        transform.position += new Vector3(Math.Sign(phase) * _UpDown, 0, 0);
+
+        if (pointer.isActive)
+        {
+            _Time = _Time + Time.deltaTime;
+            float phase = Mathf.Sin(_Time / _Period);
+            transform.position += new Vector3(Math.Sign(phase) * _UpDown, 0, 0);
+        }
     }
 }
